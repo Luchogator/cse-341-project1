425a873952ca099904ccecb6d6ed3026908ac2b5
Luis Lizano <lewislizano@gmail.com>
2025-08-03 23:35:51 -0500

    Update Swagger configuration to correct host URL and enhance Contact definition with additional properties

--- swagger.js
+++ swagger.js
@@ -7,7 +7,22 @@ const doc = {
     },
     host: 'localhost:3000',
     schemes: ['https', 'http'],
-  };
+    definitions: {
+        Contact: {
+            type: 'object',
+            properties: {
+                firstName: { type: 'string' },
+                lastName: { type: 'string' },
+                email: { type: 'string' },
+                favoriteColor: { type: 'string' },
+                birthday: { type: 'string' },
+                username: { type: 'string' },
+                name: { type: 'string' },
+                ipAddress: { type: 'string' }
+            }
+        }
+    }
+};
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];

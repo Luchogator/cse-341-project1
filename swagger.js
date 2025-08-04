6605460b7789541a73e561276821ac3a6c119e9b
Luis Lizano <lewislizano@gmail.com>
2025-08-03 23:43:52 -0500

    Refactor Swagger configuration by removing HTTPS scheme and unnecessary definitions for Contact

--- swagger.js
+++ swagger.js
@@ -6,22 +6,7 @@ const doc = {
         description: 'API for managing contacts'
     },
     host: 'localhost:3000',
-    schemes: ['https', 'http'],
-    definitions: {
-        Contact: {
-            type: 'object',
-            properties: {
-                firstName: { type: 'string' },
-                lastName: { type: 'string' },
-                email: { type: 'string' },
-                favoriteColor: { type: 'string' },
-                birthday: { type: 'string' },
-                username: { type: 'string' },
-                name: { type: 'string' },
-                ipAddress: { type: 'string' }
-            }
-        }
-    }
+    schemes: ['http']
 };
 
 const outputFile = './swagger.json';
